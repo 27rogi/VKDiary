6f4e2b82c92c275deff54ad1e15f58180c89af64
Igor Ryzhenkov <gamesforevaigor@gmail.com>
2021-01-21 21:01:01 +0300

    refactor: small improvements to minimize errors

--- src/core/PluginLoader.ts
+++ src/core/PluginLoader.ts
@@ -2,13 +2,13 @@ import fs from 'fs';
 import { IBasePlugin } from './classes/BasePlugin';
 import Logger from './utils/Logger';
 import VKClient from './VKClient';
-
 export interface PluginLoaderOptions {
-    [key: string]: string
+    pluginsDir?: string,
+    fileExt?: string
 }
-
+ 
 export default class PluginLoader {
-    pluginsList: any[];
+    pluginsList: IBasePlugin[] = [];
     private options: PluginLoaderOptions = {};
 
     constructor(pluginsDir: string, fileExt: string) {
@@ -18,9 +18,10 @@ export default class PluginLoader {
 
     async load() {
         Logger.info(`Started searching plugins in ${this.options.pluginsDir} with extension ${this.options.fileExt}`);
-        this.pluginsList = fs.readdirSync(`${this.options.pluginsDir}`).filter((file) => file.endsWith(this.options.fileExt));
-        Logger.info(`Found ${this.pluginsList.length} plugins, attempting to load them...`)
-        for (const pluginFile of this.pluginsList) {
+        const files = fs.readdirSync(`${this.options.pluginsDir}`).filter((file) => file.endsWith(this.options.fileExt));
+
+        Logger.info(`Found ${files.length} plugins, attempting to load them...`)
+        for (const pluginFile of files) {
             import(`${this.options.pluginsDir}/${pluginFile}`).then((res) => {
                 const plugin: IBasePlugin = new res.default(VKClient);
                 if (typeof plugin.execute !== 'function') return Logger.error(`Pliugin ${pluginFile} can not be loaded!`);
--- src/core/utils/Broadcaster.ts
+++ src/core/utils/Broadcaster.ts
@@ -3,20 +3,32 @@ import chats from '../database/models/chats';
 import VKClient from '../VKClient';
 import Logger from './Logger';
 
+const removedChatsCache: number[] = [];
+
 export default {
     async broadcastMessage(text: string) {
-        const chatsToBroadcast: any = await chats.find({
+        const chatsToBroadcast = await chats.find({
             botId: settings.global.groupId
         }).exec();
+
+        removedChatsCache.length = 0;
 
         for (const chat of chatsToBroadcast) {
             if (chat.doAnnounce) {
                 Logger.info('Broadcasting message to channel #' + chat.chatId);
-                VKClient.api.messages.send({
+                await VKClient.api.messages.send({
                     message: text,
                     peer_id: chat.chatId,
                     random_id: Math.random()
-                })
+                }).catch((err) => {
+                    if (err.code === 7) {
+                        if (!removedChatsCache.includes(chat.chatId)) {
+                            Logger.warn('Trying to send message in chat where bot was deleted, clearing info.');
+                            chats.deleteOne({ chatId: chat.chatId }).exec();
+                            removedChatsCache.push(chat.chatId);
+                        }
+                    } else Logger.error(err);
+                });
             }
         }
     }
